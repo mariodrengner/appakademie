838891e464eb81b1aa58b3e3e8102f022a5b1275
Mario Drengner <mario@drengner.de>
2025-08-13 13:24:30 +0200

    add task one

---
+++ 3.3.3.1/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String f = "!"; // renamed d to f because of doubled naming
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # appakademie
+Aufgaben
